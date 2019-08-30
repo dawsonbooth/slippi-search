3720d9dc607c227953c8b80dcfa5b4a35ec0977d
Dawson Booth <github@dawsonbooth.com>
2019-08-29 21:53:18 -0500

    Use jest with babel

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: "ts-jest/presets/js-with-ts",
+  preset: "ts-jest/presets/js-with-babel",
 
   // Run tests from one or more projects
   // projects: null,

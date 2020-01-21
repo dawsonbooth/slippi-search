95cb493d25e05075a5a3e2505e0cb7ee0b298438
Dawson Booth <github@dawsonbooth.com>
2020-01-20 21:46:27 -0600

    Fix test directory

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testRegex: "(/__test__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
